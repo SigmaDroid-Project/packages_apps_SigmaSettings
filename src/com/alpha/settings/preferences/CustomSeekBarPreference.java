42721ffec5a755a0fd129aa409d7703c6adc5032
Matt Filetto <matt.filetto@gmail.com>
2023-06-08 07:20:29 -0700

    Allow changing size of secondary qs tile labels
    * Also changed the method to hide the labels all together
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- src/com/alpha/settings/preferences/CustomSeekBarPreference.java
+++ src/com/alpha/settings/preferences/CustomSeekBarPreference.java
@@ -34,7 +34,7 @@ import com.android.settings.R;
 
 public class CustomSeekBarPreference extends Preference implements SeekBar.OnSeekBarChangeListener {
     protected final String TAG = getClass().getName();
-    private static final String SETTINGS_NS = "http://schemas.android.com/apk/res/com.android.settings";
+    private static final String SETTINGS_NS = "http://schemas.android.com/apk/res-auto";
     protected static final String ANDROIDNS = "http://schemas.android.com/apk/res/android";
 
     protected int mInterval = 1;
@@ -212,7 +212,7 @@ public class CustomSeekBarPreference extends Preference implements SeekBar.OnSee
     }
 
     protected int getSeekValue(int v) {
-        return 0 - Math.floorDiv(mMinValue - v, mInterval);
+        return (v - mMinValue) / mInterval;
     }
 
     protected String getTextValue(int v) {
