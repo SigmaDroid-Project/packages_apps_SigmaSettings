2568cf1365a7709f0141f4c11984644dddaa5ae2
Matt Filetto <matt.filetto@gmail.com>
2023-08-17 11:57:04 -0700

    replace com.alpha.* with com.sigma.* after upstream merge.
    * I missed a few refs I guess. Smh.
    
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- src/com/sigma/settings/fragments/qs/QsHeaderImageStyles.java
+++ src/com/sigma/settings/fragments/qs/QsHeaderImageStyles.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.alpha.settings.fragments.qs;
+package com.sigma.settings.fragments.qs;
 
 import android.content.ContentResolver;
 import android.content.Context;
