794c5bead056445c47e6a78a6b565416ebfdc521
LurdRaiden <everethduncan@gmail.com>
2024-02-27 21:15:57 -0500

    Implement RisingUI 2.0 [1/2]

--- src/com/sigma/settings/fragments/sound/HapticsPreferenceFragmentController.java
+++ src/com/sigma/settings/fragments/sound/HapticsPreferenceFragmentController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023 the risingOS android project
+ * Copyright (C) 2023 the SigmaDroid android project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
