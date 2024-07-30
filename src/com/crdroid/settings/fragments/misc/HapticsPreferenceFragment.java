5355780642fd19f75d18a18f8c283e27011d3c91
Matt Filetto <matt.filetto@gmail.com>
2024-07-29 17:08:54 -0700

    Move System Haptics from Sound to Misc
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- src/com/crdroid/settings/fragments/misc/HapticsPreferenceFragment.java
+++ src/com/crdroid/settings/fragments/misc/HapticsPreferenceFragment.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.crdroid.settings.fragments.sound;
+package com.crdroid.settings.fragments.misc;
 
 import android.content.Context;
 
