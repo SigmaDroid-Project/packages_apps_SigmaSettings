0c10f417d3d6ec071e80e77f8efaa6a3f2637cb4
Matt Filetto <matt.filetto@gmail.com>
2024-11-17 03:03:41 -0800

    Move System Haptics intensity settings to their own Settings category
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

---
+++ src/com/crdroid/settings/fragments/SystemHaptics.java
@@ -0,0 +1,207 @@
+/*
+ * Copyright (C) 2023 the risingOS android project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * Distributed under the License is distributed on an "AS IS" BASIS,
+ * Without warranties or conditions of any kind, either express or implied.
+ * See the License for the specific language governing permissions and
+ * Limitations under the License.
+ */
+package com.crdroid.settings.fragments;
+
+import android.content.Context;
+import android.os.Vibrator;
+import android.provider.Settings;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settingslib.core.AbstractPreferenceController;
+
+import com.crdroid.settings.preferences.CustomSeekBarPreference;
+
+import com.android.internal.util.android.VibrationUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Settings for haptics 
+ */
+public class SystemHaptics extends DashboardFragment {
+
+    private static final String TAG = "SystemHaptics";
+    private static final String KEY = "haptics_settings_category";
+    private static final String KEY_BACK_GESTURE_HAPTIC_INTENSITY = "back_gesture_haptic_intensity";
+    private static final String KEY_BRIGHTNESS_SLIDER_HAPTICS_INTENSITY = "qs_brightness_slider_haptic";
+    private static final String KEY_EDGE_SCROLLING_HAPTICS_INTENSITY = "edge_scrolling_haptics_intensity";
+    private static final String KEY_QS_HAPTICS_INTENSITY = "qs_haptics_intensity";
+    private static final String KEY_QS_TILE_HAPTICS_INTENSITY = "qs_panel_tile_haptic";
+    private static final String KEY_VOLUME_SLIDER_HAPTICS_INTENSITY = "volume_slider_haptics_intensity";
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.SIGMA;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.crdroid_settings_haptics;
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new SystemHapticsController(context));
+        return controllers;
+    }
+
+    public static class SystemHapticsController extends AbstractPreferenceController
+            implements Preference.OnPreferenceChangeListener {
+
+        private CustomSeekBarPreference mBackIntensity;
+        private CustomSeekBarPreference mBrightnessIntensity;
+        private CustomSeekBarPreference mEdgeScrollingIntensity;
+        private CustomSeekBarPreference mQsIntensity;
+        private CustomSeekBarPreference mQsTileIntensity;
+        private CustomSeekBarPreference mVolumeSliderIntensity;
+
+        private Context mContext;
+        private Vibrator mVibrator;
+
+        public SystemHapticsController(Context context) {
+            super(context);
+            mContext = context;
+            mVibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        }
+
+        @Override
+        public boolean isAvailable() {
+            return mVibrator != null && mVibrator.hasVibrator();
+        }
+
+        @Override
+        public String getPreferenceKey() {
+            return KEY;
+        }
+
+        @Override
+        public void displayPreference(PreferenceScreen screen) {
+            super.displayPreference(screen);
+            mBackIntensity = screen.findPreference(KEY_BACK_GESTURE_HAPTIC_INTENSITY);
+            mBrightnessIntensity = screen.findPreference(KEY_BRIGHTNESS_SLIDER_HAPTICS_INTENSITY);
+            mEdgeScrollingIntensity = screen.findPreference(KEY_EDGE_SCROLLING_HAPTICS_INTENSITY);
+            mQsIntensity = screen.findPreference(KEY_QS_HAPTICS_INTENSITY);
+            mQsTileIntensity = screen.findPreference(KEY_QS_TILE_HAPTICS_INTENSITY);
+            mVolumeSliderIntensity = screen.findPreference(KEY_VOLUME_SLIDER_HAPTICS_INTENSITY);
+            updateSettings();
+        }
+
+        private void updateSettings() {
+            int backIntensity = Settings.Secure.getInt(mContext.getContentResolver(),
+                    Settings.Secure.BACK_GESTURE_HAPTIC_INTENSITY, 2);
+            mBackIntensity.setValue(backIntensity);
+
+            int brightnessIntensity = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.QS_BRIGHTNESS_SLIDER_HAPTIC, 2);
+            mBrightnessIntensity.setValue(brightnessIntensity);
+
+            int edgeScrollingIntensity = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.EDGE_SCROLLING_HAPTICS_INTENSITY, 2);
+            mEdgeScrollingIntensity.setValue(edgeScrollingIntensity);
+
+            int volumeSliderIntensity = Settings.System.getInt(mContext.getContentResolver(),
+                    KEY_VOLUME_SLIDER_HAPTICS_INTENSITY, 1);
+            mVolumeSliderIntensity.setValue(volumeSliderIntensity);
+
+            int qsTileHapticsIntensity = Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.QS_PANEL_TILE_HAPTIC, 1);
+            mQsTileIntensity.setValue(qsTileHapticsIntensity);
+
+            int qsHapticsIntensity = Settings.System.getInt(mContext.getContentResolver(),
+                    KEY_QS_HAPTICS_INTENSITY, 1);
+            mQsIntensity.setValue(qsHapticsIntensity);
+
+            mBackIntensity.setOnPreferenceChangeListener(this);
+            mBrightnessIntensity.setOnPreferenceChangeListener(this);
+            mEdgeScrollingIntensity.setOnPreferenceChangeListener(this);
+            mQsIntensity.setOnPreferenceChangeListener(this);
+            mQsTileIntensity.setOnPreferenceChangeListener(this);
+            mVolumeSliderIntensity.setOnPreferenceChangeListener(this);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            int intensity = (Integer) newValue;
+            boolean isChanged = false;
+
+            if (preference == mBackIntensity) {
+                Settings.Secure.putInt(mContext.getContentResolver(),
+                        Settings.Secure.BACK_GESTURE_HAPTIC_INTENSITY, intensity);
+                mBackIntensity.setValue(intensity);
+                isChanged = true;
+            } else if (preference == mBrightnessIntensity) {
+                Settings.System.putInt(mContext.getContentResolver(),
+                        Settings.System.QS_BRIGHTNESS_SLIDER_HAPTIC, intensity);
+                mBrightnessIntensity.setValue(intensity);
+                isChanged = true;
+            } else if (preference == mEdgeScrollingIntensity) {
+                Settings.System.putInt(mContext.getContentResolver(),
+                        Settings.System.EDGE_SCROLLING_HAPTICS_INTENSITY, intensity);
+                mEdgeScrollingIntensity.setValue(intensity);
+                isChanged = true;
+            } else if (preference == mQsIntensity) {
+                Settings.System.putInt(mContext.getContentResolver(),
+                        KEY_QS_HAPTICS_INTENSITY, intensity);
+                mQsIntensity.setValue(intensity);
+                isChanged = true;
+            } else if (preference == mQsTileIntensity) {
+                Settings.System.putInt(mContext.getContentResolver(),
+                        Settings.System.QS_PANEL_TILE_HAPTIC, intensity);
+                mQsTileIntensity.setValue(intensity);
+                isChanged = true;
+            } else if (preference == mVolumeSliderIntensity) {
+                Settings.System.putInt(mContext.getContentResolver(),
+                        KEY_VOLUME_SLIDER_HAPTICS_INTENSITY, intensity);
+                mVolumeSliderIntensity.setValue(intensity);
+                isChanged = true;
+            }
+
+            if (isChanged) {
+                VibrationUtils.triggerVibration(mContext, intensity);
+                return true;
+            }
+
+            return false;
+        }
+    }
+
+    public static void reset(Context mContext) {
+        Settings.Secure.putInt(mContext.getContentResolver(),
+                Settings.Secure.BACK_GESTURE_HAPTIC_INTENSITY, 2);
+        Settings.System.putInt(mContext.getContentResolver(),
+                Settings.System.QS_BRIGHTNESS_SLIDER_HAPTIC, 2);
+        Settings.System.putInt(mContext.getContentResolver(),
+                Settings.System.EDGE_SCROLLING_HAPTICS_INTENSITY, 2);
+        Settings.System.putInt(mContext.getContentResolver(),
+                Settings.System.QS_PANEL_TILE_HAPTIC, 1);
+        Settings.System.putInt(mContext.getContentResolver(),
+                KEY_QS_HAPTICS_INTENSITY, 1);
+        Settings.System.putInt(mContext.getContentResolver(),
+                KEY_VOLUME_SLIDER_HAPTICS_INTENSITY, 1);
+    }
+}
